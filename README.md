ca8674f9dde4dd9c4ea7ae833192488f5b1ed14a
dl233 <38434170+dl233@users.noreply.github.com>
2020-07-07 21:29:34 +0800

    修改了部分资源的cdn地址

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Nexmoes
 OneIndex 自带主题 nexmoe 的美化修改 基于Zisbusy制作的 [nexmoe主题](https://github.com/Zisbusy/OneIndex-theme) 的二次修改
 
-## 更新了什么
+## 更新了什么(2020-7-7)
 
-1. 增加了底部音乐栏APlayer播放器的支持
-2. 对文件路径优化，安装时无需更改路径一步到位~
+1. 修复了在QQ浏览器或者其他浏览器下无法正常加载mdui样式及字体的情况
+2. 修复了某些情况下左下角音乐播放器无法正常播放的问题
 
 ## 如何使用
 
@@ -16,12 +16,12 @@ OneIndex 自带主题 nexmoe 的美化修改 基于Zisbusy制作的 [nexmoe主�
 ![](https://s1.ax1x.com/2020/04/07/G2dci8.png)
 ## 演示地址
 1. [http://bendaye.mx23.top](http://bendaye.mx23.top/)
-2. [http://bendayepan.51xy.fun/](http://bendayepan.51xy.fun/)(因使用开放域名，QQ浏览器会拦截)
+2. [http://pan.bendaye.cf/](http://pan.bendaye.cf/)
 ## 调用
  [二次元图库](http://api.mtyqx.cn/) 。
  APlayer
  metingJS
-## 已知问题
-1. 因为metingJS调用的API存在SSL证书问题，已知QQ浏览器、猎豹浏览器以及微软自家浏览器的安全策略会阻止相关请求，所以音乐插件无法正常显示。
-2. 视频播放过慢，建议使用多线程播放器PotPlayer或者QQ影音，点击视频页面下载按钮后复制下载链接到播放器里，选择打开URL的方式播放。
-3. 同理下载大型文件缓慢可以使用多线程下载工具IDM或者FDM
+## 你需要知道的东西
+1. 视频播放过慢，建议使用多线程播放器PotPlayer或者QQ影音，右键视频页面下载按钮后复制下载链接到播放器里，选择打开URL的方式播放。
+2. 同理下载大型文件缓慢可以使用多线程下载工具IDM或者FDM
+3. 因主题精简了对于图片和视频的展示，更专注于视频和图片内容的展示，引用连接部分已删除，引用链接直接复制下载按钮即可。
--- nexmoes/layout.php
+++ nexmoes/layout.php
@@ -5,9 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
 	<title><?php e(config('site_name'));?> - 私人云服务</title>
 	<link rel="shortcut icon" href="./view/nexmoes/theme/favicon.ico">
-	<link rel="stylesheet" href="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/css/mdui.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">
 	<link rel="stylesheet" href="./view/nexmoes/theme/style.css">
-	<script src="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/mdui@0.4.3/dist/js/mdui.min.js"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css">
 	<script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/meting@2/dist/Meting.min.js"></script>
@@ -18,7 +18,7 @@
 			<a href="/"><img class="avatar" src="./view/nexmoes/picture/logo.jpg" /></a>
 			<div class="navRight">
 				<ul class="navul">
-					<li class="navli"><a href="//sjdnsadq.xg2.leapidc.xyz" target="_blank">博客</a></li>
+					<li class="navli"><a href="//bilibili2233.cf" target="_blank">博客</a></li>
 					<li class="navli"><a href="./?/login">登陆</a></li>
 				</ul>
 				<div class="icon"></div>
